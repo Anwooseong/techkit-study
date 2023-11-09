bf89cfb21393e1798763c0cff32e8cf0affb8875
Anwooseong <aws5624@naver.com>
2023-11-09 16:12:25 +0900

    백준9375(패션왕 신해빈)-실버3

---
+++ src/main/java/backjoon/week2/No9375.java
@@ -0,0 +1,35 @@
+package backjoon.week2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class No9375 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+            int count = scanner.nextInt();
+            scanner.nextLine();
+
+            if (count == 0) {
+                System.out.println(0);
+                continue;
+            }
+            Map<String, Integer> clothes = new HashMap<>();
+            for (int j = 0; j < count; j++) {
+                String[] cloth = scanner.nextLine().split(" ");
+                clothes.put(cloth[1], clothes.getOrDefault(cloth[1], 0) + 1);
+            }
+
+            int sum = 1;
+            for (String clothesCount : clothes.keySet()) {
+                sum *= (clothes.get(clothesCount) + 1);
+            }
+            System.out.println(sum - 1);
+        }
+    }
+}
