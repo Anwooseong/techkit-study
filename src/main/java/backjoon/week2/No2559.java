52f160b9e07cbf76c24cfd44fce12ee4cef835c5
Anwooseong <aws5624@naver.com>
2023-11-09 15:41:01 +0900

    백준2559(수열)-실버3

---
+++ src/main/java/backjoon/week2/No2559.java
@@ -0,0 +1,28 @@
+package backjoon.week2;
+
+import java.util.Scanner;
+
+public class No2559 {
+    static int max_value = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] temperatureArray = new int[N];
+        for (int i = 0; i < N; i++) {
+            temperatureArray[i] = scanner.nextInt();
+        }
+        scanner.nextLine();
+        for (int i = 0; i < N - K + 1; i++) {
+            int sum = 0;
+            for (int j = i; j < i + K; j++) {
+                sum += temperatureArray[j];
+            }
+            max_value = Math.max(max_value, sum);
+        }
+        System.out.println(max_value);
+    }
+}
