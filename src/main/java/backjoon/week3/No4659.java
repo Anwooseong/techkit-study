650c9d391a22704fe9a5be05ad4cee9cf270baf6
Anwooseong <aws5624@naver.com>
2023-11-15 18:06:18 +0900

    백준4659(비밀번호 발음하기)-실버5

--- src/main/java/backjoon/week3/No4659.java
+++ src/main/java/backjoon/week3/No4659.java
@@ -39,7 +39,7 @@ public class No4659 {
                     repeatSum = 1;
                 }
                 if (prevAlphabet == alphabet) {
-                    if (alphabet == 'e' || alphabet == 'e') {
+                    if (alphabet == 'e' || alphabet == 'o') {
                         //허용
                         isSuccess = true;
                     } else {
