9037caf0d76769ed3430cdc050ad6c91c7482e21
Anwooseong <aws5624@naver.com>
2023-11-15 18:02:25 +0900

    백준4659(비밀번호 발음하기)-실버5

---
+++ src/main/java/backjoon/week3/No4659.java
@@ -0,0 +1,63 @@
+package backjoon.week3;
+
+import java.util.Scanner;
+
+public class No4659 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String gather = "aeiou";
+
+        while (!word.equals("end")) {
+            int repeatSum = 1;
+            char prevAlphabet = 'ㄱ';
+            boolean isSuccess = false;
+            for (int i = 0; i < word.length(); i++) {
+                char alphabet = word.charAt(i);
+                if (!isSuccess) {
+                    if (gather.contains(String.valueOf(alphabet))) {
+                        isSuccess = true;
+                    }
+                }
+                if (gather.contains(String.valueOf(alphabet)) && gather.contains(String.valueOf(prevAlphabet))) {
+                    repeatSum++;
+                    if (repeatSum == 3) {
+                        //허용 x
+                        isSuccess = false;
+                        break;
+                    }
+                } else if (!gather.contains(String.valueOf(alphabet)) && !gather.contains(String.valueOf(prevAlphabet))) {
+                    repeatSum++;
+                    if (repeatSum == 3) {
+                        //허용 x
+                        isSuccess = false;
+                        break;
+                    }
+                } else {
+                    isSuccess = true;
+                    repeatSum = 1;
+                }
+                if (prevAlphabet == alphabet) {
+                    if (alphabet == 'e' || alphabet == 'e') {
+                        //허용
+                        isSuccess = true;
+                    } else {
+                        //허용 x
+                        isSuccess = false;
+                        break;
+                    }
+                }
+                prevAlphabet = alphabet;
+            }
+            if (isSuccess) {
+                System.out.println("<" + word + "> is acceptable.");
+            } else {
+                System.out.println("<" + word + "> is not acceptable.");
+            }
+            word = scanner.nextLine();
+
+
+        }
+    }
+}
