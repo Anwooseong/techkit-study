f40f729646a4d4ba5e136476f8f8acda3afb2357
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2023-11-15 18:42:44 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 232 ms, Memory: 18296 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String gather = "aeiou";
+
+        while (!word.equals("end")) {
+            int repeatSum = 1;
+            char prevAlphabet = 'ㄱ';
+            boolean isSuccess = false;
+            for (int i = 0; i < word.length(); i++) {
+                char alphabet = word.charAt(i);
+                //최소 모음 하나
+                if (i == 0) {
+                    if (!isSuccess) {
+                        if (gather.contains(String.valueOf(alphabet))) {
+                            isSuccess = true;
+                        }
+                    }
+                }else{
+                    if (!isSuccess) {
+                        if (gather.contains(String.valueOf(alphabet))) {
+                            isSuccess = true;
+                        }
+                    }
+                    //같은 글자 2번 연속 x, ee와 oo 허용
+                    if (repeatSum == 1 && prevAlphabet == alphabet) {
+                        if (alphabet == 'e' || alphabet == 'o') {
+                            //허용
+                            repeatSum++;
+                            isSuccess = true;
+                        } else {
+                            //허용 x
+                            isSuccess = false;
+                            break;
+                        }
+                    }
+                    //모음이 3개 연속으로 올때
+                    else if (gather.contains(String.valueOf(alphabet)) && gather.contains(String.valueOf(prevAlphabet))) {
+                        repeatSum++;
+                        if (repeatSum == 3) {
+                            //허용 x
+                            isSuccess = false;
+                            break;
+                        }
+                    }
+                    //자음이 3개 연속으로 올때
+                    else if (!gather.contains(String.valueOf(alphabet)) && !gather.contains(String.valueOf(prevAlphabet))) {
+                        repeatSum++;
+                        if (repeatSum == 3) {
+                            //허용 x
+                            isSuccess = false;
+                            break;
+                        }
+                    } else {
+                        repeatSum = 1;
+                    }
+                }
+
+                prevAlphabet = alphabet;
+            }
+            if (isSuccess) {
+                System.out.println("<" + word + "> is acceptable.");
+            } else {
+                System.out.println("<" + word + "> is not acceptable.");
+            }
+            word = scanner.nextLine();
+
+
+        }
+    }
+}
