5524eec1c2635936c210d264c30ead5796dfcd6d
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2023-12-13 16:47:20 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 232 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+class Cow{
+    int arriveTime;
+    int searchTime;
+
+    public Cow(int arriveTime, int searchTime) {
+        this.arriveTime = arriveTime;
+        this.searchTime = searchTime;
+    }
+}
+
+public class Main {
+    static int N;
+    static Cow[] cows;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        cows = new Cow[N];
+        for (int i = 0; i < N; i++) {
+            cows[i] = new Cow(sc.nextInt(), sc.nextInt());
+        }
+
+        Arrays.sort(cows, (c1, c2) -> c1.arriveTime - c2.arriveTime);
+
+        int cnt = 0;
+        int answer = 0;
+        while (cnt < N) {
+            if (cows[cnt].arriveTime > answer) {
+                answer++;
+            } else {
+                answer += cows[cnt].searchTime;
+                cnt++;
+            }
+        }
+        System.out.println(answer);
+
+
+    }
+}
