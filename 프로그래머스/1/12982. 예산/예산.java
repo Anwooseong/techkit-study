585fa681d10e6728e8bbc2c1f24a88155fddaf30
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2024-05-01 14:24:03 +0900

    [level 1] Title: 예산, Time: 0.50 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,24 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
-    
     public int solution(int[] d, int budget) {
         int answer = 0;
-        Arrays.sort(d);
-        
-        int sum = 0;
-        int cnt = 1;
-        for(int i = 0; i < d.length; i++){
-            sum += d[i];
-            if(sum > budget){
-                break;
-            }
-            answer = Math.max(answer, cnt);
-            cnt++;
-        }
-        
         return answer;
     }
-    
 }
