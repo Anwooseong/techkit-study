3321f2e3c20783c9697ad1050e2a757767da5954
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2024-05-01 15:21:52 +0900

    [level 3] Title: 스티커 모으기(2), Time: 5.89 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,27 +1,10 @@
 class Solution {
-    int[] dp;
     public int solution(int sticker[]) {
         int answer = 0;
 
-        int N = sticker.length;
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
-        if(N == 1)return sticker[0];
-        if(N == 2)return Math.max(sticker[0], sticker[1]);
-        dp = new int[N];
-        dp[0] = dp[1] = sticker[0];
-        for(int i = 2; i < N; i++){
-            dp[i] = Math.max(dp[i-2]+sticker[i], dp[i-1]);
-        }
-        int firstAnswer = dp[N-2];
-        
-        dp = new int[N];
-        dp[1] = sticker[1];
-        for(int i = 2; i < N; i++){
-            dp[i] = Math.max(dp[i-2]+sticker[i], dp[i-1]);
-        }
-        int secondAnswer = dp[N-1];
-        
-        
-        return Math.max(firstAnswer, secondAnswer);
+        return answer;
     }
 }
