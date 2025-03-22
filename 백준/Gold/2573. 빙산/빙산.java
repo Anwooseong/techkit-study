592dde59106ca34ecd9b3d2701846d7f58414685
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2025-03-22 19:24:18 +0900

    [Gold IV] Title: 빙산, Time: 420 ms, Memory: 135044 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -4,103 +4,93 @@ import java.io.*;
 public class Main {
 
     static int N, M;
-    static int[][] map;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int[][] originMap, copyMap;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         init();
 
         int year = 0;
         while (true) {
-            int cnt = countLand();
-            if (cnt >= 2) {
-                break;
-            } else if (cnt == 0) {
-                year = 0;
-                break;
-            }
-            bfs();
-            year++;
-        }
-        System.out.println(year);
-    }
-
-    static void bfs() {
-        boolean[][] visited = new boolean[N][M];
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] != 0) {
-                    queue.add(new int[]{i, j});
-                    visited[i][j] = true;
-                }
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            int x = poll[0];
-            int y = poll[1];
             int cnt = 0;
+            copyMap = copy();
+            visited = new boolean[N][M];
 
-            for (int i = 0; i < 4; i++) {
-                int nextX = x + dx[i];
-                int nextY = y + dy[i];
-                if (!isInRange(nextX, nextY)) continue;
-                if (visited[nextX][nextY]) continue;
-                if (map[nextX][nextY] == 0) {
-                    cnt++;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (copyMap[i][j] > 0 && !visited[i][j]) {
+                        cnt++;
+                        visited[i][j] = true;
+                        dfs(i, j);
+                    }
                 }
             }
-            map[x][y] = Math.max(map[x][y] - cnt, 0);
+            year++;
+
+            if (cnt >= 2) {
+                System.out.println(year-1);
+                return;
+            } else if (cnt == 0) {
+                System.out.println(0);
+                return;
+            }
+
+            originMap = copyMap;
         }
     }
 
-    static int countLand() {
-        boolean[][] visited = new boolean[N][M];
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] != 0 && !visited[i][j]) {
-                    dfs(i, j, visited);
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-
-    static void dfs(int x, int y, boolean[][] visited) {
-        visited[x][y] = true;
+    static void dfs(int x, int y) {
+        int zeroCnt = 0;
         for (int i = 0; i < 4; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
             if (!isInRange(nextX, nextY)) continue;
+            if (originMap[nextX][nextY] == 0) zeroCnt++;
+        }
+        copyMap[x][y] = Math.max(copyMap[x][y] - zeroCnt, 0);
 
-            if (map[nextX][nextY] != 0 && !visited[nextX][nextY]) {
-                dfs(nextX, nextY, visited);
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if (!isInRange(nextX, nextY)) continue;
+            if (originMap[nextX][nextY] == 0) continue;
+            if (visited[nextX][nextY]) continue;
+
+            visited[nextX][nextY] = true;
+            dfs(nextX, nextY);
+        }
+
+    }
+
+    static int[][] copy() {
+        int[][] copyMap = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                copyMap[i][j] = originMap[i][j];
             }
         }
+        return copyMap;
     }
 
     static boolean isInRange(int x, int y) {
         return x >= 0 && x < N && y >= 0 && y < M;
     }
 
-    static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    private static void init() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        st = new StringTokenizer(bf.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
+        originMap = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(bf.readLine(), " ");
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                originMap[i][j] = Integer.parseInt(st.nextToken());
             }
         }
     }
