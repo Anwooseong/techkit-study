ab5a08355367923091e9c0fc77258d5162cefead
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2025-05-21 18:57:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 532 ms, Memory: 95944 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,52 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] answer = new int[N];
-        answer[0] = arr[0];
-        int lastIndex = 1;
-        for (int i = 1; i < N; i++) {
-            if (arr[i] > answer[lastIndex - 1]) {
-                answer[lastIndex] = arr[i];
-                lastIndex++;
-            } else {
-                int left = 0;
-                int right = lastIndex;
-
-                while (left < right) {
-                    int mid = (left + right) / 2;
-
-                    if (arr[i] > answer[mid]) {
-                        left = mid + 1;
-                    } else { //arr[i] >= arr[mid]
-                        right = mid;
-                    }
-                }
-
-                answer[left] = arr[i];
-            }
-        }
-
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            if (answer[i] == 0) {
-                break;
-            }
-            cnt++;
-        }
-        System.out.println(cnt);
-    }
+	
+	static int N, lengthOfLIS;
+	static int[] arr, LIS;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(bf.readLine());
+		arr = new int[N];
+		LIS = new int[N];
+		
+		st = new StringTokenizer(bf.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		LIS[0] = arr[0];
+		lengthOfLIS = 1;
+		
+		for(int i = 1; i < N; i++) {
+			int num = LIS[lengthOfLIS - 1];
+			
+			if(num < arr[i]) {
+				lengthOfLIS++;
+				LIS[lengthOfLIS - 1] = arr[i];
+			}else {
+				int idx = binarySearch(arr[i]);
+//				System.out.println(i+"/"+"idx="+idx);
+				LIS[idx] = arr[i];
+			}
+		}
+		
+		System.out.println(lengthOfLIS);
+	}
+	
+	private static int binarySearch(int num) {
+		int left = 0;
+		int right = lengthOfLIS - 1;
+		
+		while(left <= right) {
+			int mid = (left + right) / 2;
+		
+			if(num > LIS[mid]) {
+				left = mid + 1;
+//				System.out.println(left);
+			}else {
+				right = mid - 1;
+//				System.out.println(right);
+			}
+		}
+		
+		return right + 1;
+	}
 }
+
+
