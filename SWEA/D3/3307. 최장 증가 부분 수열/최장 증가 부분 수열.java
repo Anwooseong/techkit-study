7f01a3e2a5f1ad4b2751cba64bb960d19ce1d9fc
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2025-05-21 17:48:22 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 134 ms, Memory: 34,372 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -2,36 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
+	
+	static int T, N, answer;
+	static int[] arr, dp;
+	
+	public static void main(String[] args) throws Exception{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(bf.readLine());
-		
 		StringTokenizer st;
+		
+		T = Integer.parseInt(bf.readLine());
 		for(int testCase = 1; testCase <= T; testCase++) {
-			int N = Integer.parseInt(bf.readLine());
+			N = Integer.parseInt(bf.readLine());
+			arr = new int[N];
+			dp = new int[N];
+			answer = 0;
 			
-			st = new StringTokenizer(bf.readLine()," ");
-			int[] arr = new int[N];
-			int[] dp = new int[N];
+			st = new StringTokenizer(bf.readLine(), " ");
 			for(int i = 0; i < N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			
 			dp[0] = 1;
-			int max = 1;
 			for(int i = 1; i < N; i++) {
 				dp[i] = 1;
 				for(int j = 0; j < i; j++) {
-					if(arr[j] < arr[i] && dp[i] <= dp[j]) {
+					if(arr[i] > arr[j] && dp[j] >= dp[i]) {
 						dp[i] = dp[j] + 1;
 					}
 				}
+				
+				answer = Math.max(answer, dp[i]);
 			}
-			for(int num : dp) {
-				max = Math.max(num, max);
-			}
-			System.out.println("#"+testCase+" "+max);
+			
+			System.out.printf("#%d %d\n", testCase, answer);
 		}
 	}
 }
